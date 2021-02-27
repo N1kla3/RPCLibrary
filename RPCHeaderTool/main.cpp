7f016e02f056bdc999a95cce16a39d3baea3e4f2
Nicola <kolya.vladimirsky@gmail.com>
2021-02-27 10:18:51 +0300

    header tool definition

--- RPCHeaderTool/main.cpp
+++ RPCHeaderTool/main.cpp
@@ -2,21 +2,34 @@
 // Created by nicola on 26/02/2021.
 
 #include <fstream>
+#include <iostream>
 
 using std::string;
 
+void GenerateHeader(const string& pathToProjectSource, const string& whereToGenerate);
+void GenerateCpp(const string& pathToProjectSource, const string& whereToGenerate);
+
+// first param - path to files for parsing
+// second param - path to output folder
 int main(int arg, char** argc)
 {
-	if (arg != 2)
+	if (arg != 3)
 	{
 		return -1;
 	}
-	string path_to_folder = static_cast<string>(*argc);
-	auto header_path = path_to_folder.append("/generated.h");
-	std::ofstream header(header_path);
-	header.close();
-	auto cpp_path = path_to_folder.append("/generated.cpp");
-	std::ofstream cpp(cpp_path);
-	cpp.close();
+	string path_to_source = static_cast<string>(argc[0]);
+	string path_to_generate_folder = static_cast<string>(argc[1]);
+	GenerateHeader(path_to_source, path_to_generate_folder);
+	GenerateCpp(path_to_source, path_to_generate_folder);
 	return 0;
+}
+
+void GenerateHeader(const string& pathToProjectSource, const string& whereToGenerate)
+{
+
+}
+
+void GenerateCpp(const string& pathToProjectSource, const string& whereToGenerate)
+{
+
 }
